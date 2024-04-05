372ef6998580e29f2191e7795ee24bca192de707
Gürkan Sevil <sevilgurkann@gmail.com>
2024-04-05 23:59:40 +0300

    feat(client): add constants

---
+++ client/src/constants/api-paths.ts
@@ -0,0 +1,7 @@
+export const API_PATHS = {
+  GLOBAL_PAGE: "/api/global",
+  HOME_PAGE: "/api/home-page",
+  GET_ME: "/api/users/me",
+  REGISTER_LOCAL: "/api/auth/local/register",
+  LOGIN_LOCAL: "/api/auth/local",
+} as const
---
+++ client/src/constants/component-names.ts
@@ -0,0 +1,4 @@
+export const COMPONENT_NAMES = {
+  LAYOUT_HERO_SECTION: "layout.hero-section",
+  LAYOUT_FEATURES_SECTION: "layout.features-section",
+} as const

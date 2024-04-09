f9ba145cda9ceafedcc0d19ca4314af3cc78b4e1
Gürkan Sevil <sevilgurkann@gmail.com>
2024-04-09 23:05:05 +0300

    feat(client): add loaders

--- client/src/loaders/index.ts
+++ client/src/loaders/index.ts
@@ -1,3 +1,5 @@
+import { unstable_noStore as noStore } from "next/cache"
+
 import { fetchData } from "@/lib/fetch-data"
 import { API_PATHS } from "@/constants/api-paths"
 
@@ -9,4 +11,46 @@ export async function getGlobalPageMetadata() {
   }
 
   return await fetchData(API_PATHS.GLOBAL_PAGE, urlParamsObject)
+}
+
+export async function getPageBySlug(slug: string, lang: string) {
+  noStore()
+
+  const urlParamsObject = {
+    filters: {
+      slug: {
+        $eq: slug,
+      },
+    },
+    populate: {
+      blocks: {
+        populate: {
+          links: true,
+          imageLinks: {
+            populate: {
+              image: {
+                fields: ["url"],
+              },
+            },
+          },
+          products: {
+            populate: {
+              price: true,
+              brand: {
+                fields: ["name"],
+              },
+              category: {
+                fields: ["name"],
+              },
+              images: {
+                fields: ["url", "alternativeText"],
+              },
+            },
+          },
+        },
+      },
+    },
+  }
+
+  return await fetchData(API_PATHS.PAGES, urlParamsObject)
 }

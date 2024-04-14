2d91a5a86754b7ea97eaae55a0e0cb15604e57a9
Gürkan Sevil <sevilgurkann@gmail.com>
2024-04-14 23:44:46 +0300

    chore(client): add types for unknown valid pathnames

--- client/src/i18n/navigation.ts
+++ client/src/i18n/navigation.ts
@@ -4,6 +4,6 @@ import { locales, pathnames, localePrefix } from "./config"
 export const { Link, redirect, usePathname, useRouter } =
   createLocalizedPathnamesNavigation({
     locales,
-    pathnames,
+    pathnames: pathnames as typeof pathnames & Record<string & {}, string>,
     localePrefix,
   })

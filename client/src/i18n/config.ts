338803fd7921bf83245f6f5b8bf45e33654395d9
Gürkan Sevil <sevilgurkann@gmail.com>
2024-05-02 22:39:09 +0300

    chore(client): add pathnames to i18 config

--- client/src/i18n/config.ts
+++ client/src/i18n/config.ts
@@ -3,7 +3,16 @@ import { Pathnames } from "next-intl/navigation"
 export const locales = ["tr", "en"] as const
 export const defaultLocale = "tr"
 
-export const pathnames = {} satisfies Pathnames<typeof locales>
+export const pathnames = {
+  "/category/[slug]": {
+    tr: "/kategori/[slug]",
+    en: "/category/[slug]",
+  },
+  "/product/[slug]": {
+    tr: "/urun/[slug]",
+    en: "/product/[slug]",
+  },
+} satisfies Pathnames<typeof locales>
 
 export const localePrefix = "as-needed"
 
